5bc74969f371a0a97815f24baea5be085dedeb6e
spacegeek224 <marisusisme@Gmail.com>
2016-01-31 23:41:59 +0000

    help command

--- src/main/java/net/spacegeek224/AdminTools/Main.java
+++ src/main/java/net/spacegeek224/AdminTools/Main.java
@@ -1,7 +1,13 @@
-package net.spacegeek224.blankplugin;
+package net.spacegeek224.AdminTools;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 
 public class Main extends JavaPlugin {
 	
@@ -14,5 +20,20 @@ public class Main extends JavaPlugin {
 	public void onDisable() {
 		getLogger().info(ChatColor.RED + "Plugin disabled!");
 	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if(commandLabel.equalsIgnoreCase("help")){
+
+            //Defines Variables
+//             Player p = (Player) sender;
+
+            //Send message
+            sender.sendMessage(ChatColor.GOLD + "SpaceHex Network");
+            sender.sendMessage(ChatColor.WHITE + "====================================================");
+            sender.sendMessage(ChatColor.AQUA + "/help" + ChatColor.WHITE + " - Displays help");
+            sender.sendMessage(ChatColor.GRAY + "====================================================");
+        }
+        return false;
+    }
 	
 }
