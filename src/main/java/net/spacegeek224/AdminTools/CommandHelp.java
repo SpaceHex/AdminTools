e2841a42164b14e7ae35c9ff34aa18b9096f83d4
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-04 13:34:49 -0500

    Update CommandHelp.java

--- src/main/java/net/spacegeek224/AdminTools/CommandHelp.java
+++ src/main/java/net/spacegeek224/AdminTools/CommandHelp.java
@@ -10,13 +10,14 @@ import org.bukkit.entity.Player;
 public class CommandHelp implements CommandExecutor {                                                                    
         @Override                                                                                                        
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {                       
-                Player player = (Player) sender;
+                
+                //Player player = (Player) sender;
                 sender.sendMessage(ChatColor.GOLD + "SpaceHex Network");
                 sender.sendMessage(ChatColor.WHITE + "====================================================");
-                sender.sendMessage(ChatColor.AQUA + "/help" + ChatColor.WHITE + " - Displays help.");
-                sender.sendMessage(ChatColor.AQUA + "/hub" + ChatColor.WHITE + " - Go to the hub.");
-	        sender.sendMessage(ChatColor.AQUA + "/report <player> <reason>" + ChatColor.WHITE + " - File a report.");
-                sender.sendMessage(ChatColor.GRAY + "====================================================");
+                sender.sendMessage(ChatColor.BLUE + "/help" + ChatColor.YELLOW + " - Displays help.");
+                sender.sendMessage(ChatColor.BLUE + "/hub" + ChatColor.YELLOW + " - Go to the hub.");
+	        sender.sendMessage(ChatColor.BLUE + "/report <player> <reason>" + ChatColor.YELLOW + " - File a report.");
+                sender.sendMessage(ChatColor.WHITE + "====================================================");
                 return false;                                                                                            
         }                                                                                                                
                                                                                                                          
