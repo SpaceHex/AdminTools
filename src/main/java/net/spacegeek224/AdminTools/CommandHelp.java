6f4c5731f1c68ba24b235067a0c0e38887120d45
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-05 08:53:52 -0500

    Update CommandHelp.java

--- src/main/java/net/spacegeek224/AdminTools/CommandHelp.java
+++ src/main/java/net/spacegeek224/AdminTools/CommandHelp.java
@@ -12,13 +12,15 @@ public class CommandHelp implements CommandExecutor {
         public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {                       
                 
                 //Player player = (Player) sender;
-                sender.sendMessage(ChatColor.GOLD + "SpaceHex Network");
-                sender.sendMessage(ChatColor.WHITE + "====================================================");
-                sender.sendMessage(ChatColor.BLUE + "/help" + ChatColor.YELLOW + " - Displays help.");
-                sender.sendMessage(ChatColor.BLUE + "/hub" + ChatColor.YELLOW + " - Go to the hub.");
-	        sender.sendMessage(ChatColor.BLUE + "/report <player> <reason>" + ChatColor.YELLOW + " - Report a player.");
-	        sender.sendMessage(ChatColor.BLUE + "/bug <description>" + ChatColor.YELLOW + " - File a bug report.");
-                sender.sendMessage(ChatColor.WHITE + "====================================================");
+                if (args.length == 0) {
+	                sender.sendMessage(ChatColor.GOLD + "SpaceHex Network");
+	                sender.sendMessage(ChatColor.WHITE + "====================================================");
+	                sender.sendMessage(ChatColor.BLUE + "/help" + ChatColor.YELLOW + " - Displays help.");
+	                sender.sendMessage(ChatColor.BLUE + "/hub" + ChatColor.YELLOW + " - Go to the hub.");
+		        sender.sendMessage(ChatColor.BLUE + "/report <player> <reason>" + ChatColor.YELLOW + " - Report a player.");
+		        sender.sendMessage(ChatColor.BLUE + "/bug <description>" + ChatColor.YELLOW + " - File a bug report.");
+	                sender.sendMessage(ChatColor.WHITE + "====================================================");
+                }
                 return true;                                                                                            
         }                                                                                                                
                                                                                                                          
